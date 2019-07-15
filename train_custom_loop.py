dfa811468e1dc19fd5327d79de9c3ccf4bfade36
dohyun <dhkim1028@korea.ac.kr>
2019-07-15 13:26:39 +0000

    최초 업로드

---
+++ train_custom_loop.py
@@ -0,0 +1,73 @@
+import sys
+
+sys.path.append('..')
+import matplotlib.pyplot as plt
+import numpy as np
+from RNN.optimizer import SGD
+from RNN import ptb
+from RNN.simple_rnnlm import SimpleRnnlm
+
+# 하이퍼파라미터 설정
+batch_size = 10
+wordvec_size = 100
+hidden_size = 100  # RNN 은닉 상태 벡터의 원소 수
+time_size = 5  # Truncated BPTT가 한 번에 펼치는 시간 크기
+lr = 0.1
+max_epoch = 100
+
+# 학습 데이터 읽기(전체 중 1000개만)
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+corpus_size = 1000
+corpus = corpus[:corpus_size]
+vocab_size = int(max(corpus) + 1)
+
+# 입력 단어의 바로 다음 단어가 정답이 되도록 데이터 구성
+xs = corpus[:-1]  # 입력 (999개)
+ts = corpus[1:]  # 출력(정답 레이블) (999개)
+
+data_size = len(xs)
+print("말뭉치 크기 : %d, 어휘 수 : %d" % (corpus_size, vocab_size))
+
+# 학습 시 사용하는 변수
+max_iters = data_size // (batch_size * time_size)
+time_idx = 0
+total_loss = 0
+loss_count = 0
+ppl_list = []
+
+# 모델 생성
+model = SimpleRnnlm(vocab_size, wordvec_size, hidden_size)
+optimizer = SGD(lr)
+
+# 각 미니배치에서 샘플을 읽기 시작 위치를 계산
+jump = (corpus_size - 1) // batch_size
+offsets = [i * jump for i in range(batch_size)]
+
+for epoch in range(max_epoch):
+    for iter in range(max_iters):
+        # 미니배치 획득
+        batch_x = np.empty((batch_size, time_size), dtype='i')
+        batch_t = np.empty((batch_size, time_size), dtype='i')
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                batch_x[i, t] = xs[(offset + time_idx) % data_size]
+                batch_t[i, t] = ts[(offset + time_idx) % data_size]
+
+            time_idx += 1
+
+        # 기울기를 구하여 매개변수 갱신
+        loss = model.forward(batch_x, batch_t)
+        model.backward()
+        optimizer.update(model.params, model.grads)
+        total_loss += loss
+        loss_count += 1
+
+    # 에폭마다 퍼플렉서티 평가
+    ppl = np.exp(total_loss / loss_count)
+    print("|에폭 %d | 퍼플렉서티 %.2f" % (epoch + 1, ppl))
+    ppl_list.append(float(ppl))
+    total_loss, loss_count = 0, 0
+
+epochnum = list(range(len(ppl_list)))
+plt.plot(ppl_list, epochnum)
+plt.show()
